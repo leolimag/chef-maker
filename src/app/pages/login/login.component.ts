3f0a0339e0a7f74fd2ed65cb8a6119f5be4eca55
André Bueno <andrebueno99@gmail.com>
2023-06-05 11:46:07 -0300

    Correção de cor na Pagina Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth.service';
 
@@ -13,13 +13,13 @@ import { AuthService } from 'src/app/auth.service';
 
 
 
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
   title = 'chef-maker';
 
   email = '' as string;
   password = '' as string;
 
-  constructor(public auth : AuthService, public router: Router) {
+  constructor(public auth : AuthService, public router: Router, private renderer: Renderer2) {
     
   }
 
@@ -28,5 +28,13 @@ export class LoginComponent {
     this.router.navigate(['/home'])
   }
 
+
+  ngOnInit() {
+    this.renderer.addClass(document.body, 'cor-login-background');
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body, 'cor-login-background');
+  }
 
 }
